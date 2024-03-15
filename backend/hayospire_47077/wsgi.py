2bd124b06a3ffc46d4af5e3c45698e2e7f557c03
Crowdbotics <team@crowdbotics.com>
2024-03-15 08:59:34 +0000

    Initial commit

---
+++ backend/hayospire_47077/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for hayospire_47077 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "hayospire_47077.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hayospire-47077",
+  url: "https://hayospire-47077.botics.co",
+  api: "https://hayospire-47077.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
